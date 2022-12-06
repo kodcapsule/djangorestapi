55948548b6c917bc15262b42d6e7ec99ea361c93
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2022-12-06 22:38:20 +0000

    added a blog post app

--- RestAPIs/urls.py
+++ RestAPIs/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sites.urls'))
+    path('', include('sites.urls')),
+    path('blogs/api', include('blogApi.urls')),
 ]
