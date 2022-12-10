ba741429f6426003d6f2873c9f4e02db95dece04
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2022-12-10 22:01:59 +0000

    added serializer class

--- RestAPIs/urls.py
+++ RestAPIs/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sites.urls')),
-    path('blogs/api/', include('blogApi.urls')),
+    path('blogs/api/v1/', include('blogApi.urls')),
 ]
--- blogApi/urls.py
+++ blogApi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blogPost, name='blog-api')
+    path('', views.blogPost, name='blog-api'),
+    path('post/<int:id>/', views.blogPost, name='blog-api'),
 ]
